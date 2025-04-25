615791dd78f1ba645173677e3dab9971300fe7ba
Wali Queiroz <wsantos077@gmail.com>
2025-04-25 02:19:41 -0300

    feat: add tests for ExtractUserID method in JWTTokenManager

--- internal/infra/outgoing/security/jwt_token_manager_test.go
+++ internal/infra/outgoing/security/jwt_token_manager_test.go
@@ -52,3 +52,81 @@ func Test_JWTTokenManager_GetTokenType(t *testing.T) {
 		assert.Equal(t, "Bearer", tokenType)
 	})
 }
+
+func Test_JWTTokenManager_ExtractUserID(t *testing.T) {
+	t.Run("should extract user ID successfully", func(t *testing.T) {
+		// given
+		secretKey := "mysecretkey"
+		tokenManager := security.NewJWTTokenManager(secretKey)
+		expectedUserID := "some-user-id"
+
+		token := jwt.New(jwt.SigningMethodHS256)
+		token.Valid = true
+		token.Claims = jwt.MapClaims{
+			"authorized": true,
+			"exp":        time.Now().Add(time.Hour).Unix(),
+			"userID":     expectedUserID,
+		}
+
+		// when
+		userID, err := tokenManager.ExtractUserID(token)
+
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, expectedUserID, userID)
+	})
+
+	t.Run("should return error when token is not a JWT token", func(t *testing.T) {
+		// given
+		secretKey := "mysecretkey"
+		invalidToken := "invalid-token"
+
+		tokenManager := security.NewJWTTokenManager(secretKey)
+
+		// when
+		userID, err := tokenManager.ExtractUserID(invalidToken)
+
+		// then
+		assert.Error(t, err)
+		assert.Empty(t, userID)
+		assert.EqualError(t, err, "invalid token")
+	})
+
+	t.Run("should return error when token is invalid", func(t *testing.T) {
+		// given
+		secretKey := "mysecretkey"
+		tokenManager := security.NewJWTTokenManager(secretKey)
+
+		invalidToken := jwt.New(jwt.SigningMethodHS256)
+		invalidToken.Valid = false
+
+		// when
+		userID, err := tokenManager.ExtractUserID(invalidToken)
+
+		// then
+		assert.Error(t, err)
+		assert.Empty(t, userID)
+		assert.EqualError(t, err, "invalid token")
+	})
+
+	t.Run("should return error when userID claim is missing", func(t *testing.T) {
+		// given
+		secretKey := "mysecretkey"
+		tokenManager := security.NewJWTTokenManager(secretKey)
+
+		token := jwt.New(jwt.SigningMethodHS256)
+		token.Valid = true
+		token.Claims = jwt.MapClaims{
+			"authorized": true,
+			"exp":        time.Now().Add(time.Hour).Unix(),
+		}
+
+		// when
+		userID, err := tokenManager.ExtractUserID(token)
+
+		// then
+		assert.Error(t, err)
+		assert.Empty(t, userID)
+		assert.EqualError(t, err, "invalid token")
+	})
+}
